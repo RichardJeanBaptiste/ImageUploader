abe91b72d505fc9ce7526b946f3711173bf5a767
Richard Jean-Baptiste <Richinbk1@gmail.com>
2021-08-27 14:13:11 -0400

    deploying to heroku

--- server.py
+++ server.py
@@ -8,8 +8,8 @@ app = Flask(__name__, static_url_path='/', static_folder='client/build')
 @app.route("/")
 def hello_world():
     #return app.send_static_file('index.html')
-    #return render_template('/index.html')
-    return send_from_directory(app.static_folder, 'index.html')
+    return render_template('client/build/index.html')
+    #return send_from_directory(app.static_folder, 'index.html')
 
 @app.route('/image_upload/<id>', methods=['POST'])
 def upload_file(id):
