61d38a6819e598437e401904a5ed37416c1f311f
Richard Jean-Baptiste <Richinbk1@gmail.com>
2021-08-27 14:11:32 -0400

    deploying to heroku

--- server.py
+++ server.py
@@ -2,9 +2,9 @@ from flask import Flask, request, redirect, url_for, send_file, send_from_direct
 from werkzeug.utils import secure_filename
 import os
 
-app = Flask(__name__, static_url_path='/', static_folder='./client/build')
+app = Flask(__name__, static_url_path='/', static_folder='client/build')
 
-
+#client/build/index.html
 @app.route("/")
 def hello_world():
     #return app.send_static_file('index.html')
